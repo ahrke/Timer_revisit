8ac8728a5111f9b2f3d8cda420f4af05fa16287c
ahrke <david.duy.minh@gmail.com>
2017-09-29 13:10:16 +0700

    added clock, timer, and countdown. added testing

--- app/app.jsx
+++ app/app.jsx
@@ -2,16 +2,22 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
-var Nav = require('Nav');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css');
+require('style!css!sass!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 // Load CSS
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <Nav />,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+        <IndexRoute component={Timer} />
+        <Route path="countdown" component={Countdown}></Route>
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
